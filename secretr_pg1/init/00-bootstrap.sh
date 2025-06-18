d1f7c13ab840665cb977fce541fb4b0f049a968a
pain <ravi.ameer@gmail.com>
2025-06-18 23:32:46 +0530

    Changes:
    1. Working version of pg with bootstrap file and capability of pulling keys from vault.
    2. Instructions updated.

---
+++ secretr_pg1/init/00-bootstrap.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+echo "[INFO] Waiting for Vault to become available..."
+
+until curl -s http://secretr_vault1:8200/secret/postgres >/dev/null; do
+  sleep 1
+done
+
+echo "[INFO] Fetching admin password from vault..."
+ADMIN_PASS=$(curl -s http://secretr_vault1:8200/secret/postgres | jq -r .password)
+
+if [ -z "$ADMIN_PASS" ] || [ "$ADMIN_PASS" = "null" ]; then
+  echo "[ERROR] Admin password is empty or not found in vault."
+  exit 1
+fi
+
+echo "[INFO] Setting admin password and creating databases..."
+
+#TO-DO: Remove the prerequisite step of creating databases at the start.
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  ALTER USER $POSTGRES_USER WITH PASSWORD '${ADMIN_PASS}';
+  CREATE DATABASE data_a;
+  CREATE DATABASE data_b;
+EOSQL
